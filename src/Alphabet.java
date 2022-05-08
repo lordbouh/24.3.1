72081afe6d7c2b8653aa3b6a0f28a1c104b55f96
lordbouh <lordbouh@yahoo.com>
2022-05-08 16:46:24 +0300

    created 24.3.1

---
+++ src/Alphabet.java
@@ -0,0 +1,156 @@
+class Alphabet{
+        public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getO() {
+        return o;
+    }
+
+    public int getP() {
+        return p;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public int getT() {
+        return t;
+    }
+
+    public int getU() {
+        return u;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+    private int g;
+    private int h;
+    private int i;
+    private int j;
+    private int k;
+    private int l;
+    private int m;
+    private int n;
+    private int o;
+    private int p;
+    private int q;
+    private int r;
+    private int s;
+    private int t;
+    private int u;
+    private int v;
+    private int w;
+    private int x;
+    private int y;
+    private int z;
+    private int sum;
+
+    public int getSum() {
+        return a + b + c + d + e + f + g + h + i + j + k + l +
+                m + n + o + p + q + r + s + t + u + v +
+                w + x + y + z;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+}
+
+    
